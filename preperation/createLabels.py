40a5481b02ce0415a244dab157503641147b3c22
Devansh Gupta <54774124+devanshgupta160@users.noreply.github.com>
2020-11-13 20:17:02 +0530

    Update createLabels.py

--- preperation/createLabels.py
+++ preperation/createLabels.py
@@ -11,11 +11,9 @@ import numpy as np
 from numpngw import write_png
 
 from json2labelImg import json2labelImg
-from json2instanceImg import json2instanceImg
 
 
 from tqdm import tqdm
-from cityscape_panoptic_gt import panoptic_converter
 from argparse import ArgumentParser
 import os
 
@@ -34,17 +32,6 @@ def process_folder(fn):
         tqdm.write("Failed to convert: {}".format(fn))
         raise
 
-    if args.instance:
-        dst = fn.replace("_polygons.json",
-                         "_instance{}s.png".format(args.id_type))
-
-        # do the conversion
-        # try:
-        json2instanceImg(fn, dst, args.id_type)
-        # except:
-        #     tqdm.write("Failed to convert: {}".format(f))
-        #     raise
-
     if args.color:
         # create the output filename
         dst = fn.replace("_polygons.json", "_labelColors.png")
@@ -56,9 +43,6 @@ def process_folder(fn):
             tqdm.write("Failed to convert: {}".format(f))
             raise
 
-    # if args.panoptic and args.instance:
-        # panoptic_converter(f, out_folder, out_file)
-
 
 def get_args():
     parser = ArgumentParser()
@@ -66,8 +50,6 @@ def get_args():
     parser.add_argument('--datadir', default="")
     parser.add_argument('--id-type', default='level3Id')
     parser.add_argument('--color', type=bool, default=False)
-    parser.add_argument('--instance', type=bool, default=False)
-    parser.add_argument('--panoptic', type=bool, default=False)
     parser.add_argument('--num-workers', type=int, default=10)
 
     args = parser.parse_args()
@@ -79,8 +61,6 @@ def get_args():
 
 def main(args):
     import sys
-    if args.panoptic:
-        args.instance = True
     sys.path.append(os.path.normpath(os.path.join(
         os.path.dirname(__file__), '..', 'helpers')))
     # how to search for all ground truth
@@ -115,17 +95,6 @@ def main(args):
         tqdm(pool.imap(process_folder, files), total=len(files)))
     pool.close()
     pool.join()
-
-    if args.panoptic:
-        for split in ['train', 'val']:
-
-            tqdm.write("Panoptic Segmentation {} split".format(split))
-            folder_name = os.path.join(args.datadir, 'gtFine')
-            output_folder = os.path.join(folder_name, split + "_panoptic")
-            os.makedirs(output_folder, exist_ok=True)
-            out_file = os.path.join(folder_name, split + "_panoptic.json")
-            panoptic_converter(args.num_workers, os.path.join(
-                folder_name, split), output_folder, out_file)
 
 
 if __name__ == "__main__":
